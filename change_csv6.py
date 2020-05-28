db651496e25e83563afc100bef44f3a9c7a72ed7
fish <767868009@qq.com>
2020-05-28 14:27:30 +0800

    v 2.1

--- change_csv6.py
+++ change_csv6.py
@@ -1,28 +1,30 @@
 import csv
 import random
 
-file_name = 'data1/1.csv'
-for x_1 in range(2, 8):
-    for x_2 in range(x_1 + 1, 8):
-        for x_3 in range(x_2 + 1, 8):
-            for x_4 in range(x_3 + 1, 8):
-                for x_5 in range(x_4 + 1, 8):
-                    for x_6 in range(x_5 + 1, 8):
-                        change_cols = [x_1, x_2, x_3, x_4, x_5, x_6]
-                        for i in range(5):
+for s in ['a', 'b', 'c']:
+    file_name = 'data2/' + s + '.csv'
+    for x_1 in range(2, 8):
+        for x_2 in range(x_1 + 1, 8):
+            for x_3 in range(x_2 + 1, 8):
+                for x_4 in range(x_3 + 1, 8):
+                    for x_5 in range(x_4 + 1, 8):
+                        for x_6 in range(x_5 + 1, 8):
+                            change_cols = [x_1, x_2, x_3, x_4, x_5, x_6]
+                            for i in range(5):
 
-                            out_file = 'data/6/6-' + ''.join(str(x) for x in change_cols) + '-' + str(i) + '.csv'
-                            data = []
-                            with open(file_name) as file:
-                                file_csv = csv.reader(file)
-                                for row in file_csv:
-                                    # print(row)
+                                out_file = 'data3/' + s + '/6/' + ''.join(str(x) for x in change_cols) + '-' + str(
+                                    i) + '.csv'
+                                data = []
+                                with open(file_name) as file:
+                                    file_csv = csv.reader(file)
+                                    for row in file_csv:
+                                        # print(row)
 
-                                    for col in change_cols:
-                                        row[col] = int(row[col]) + random.randint(-5, 5)
-                                    # print(row)
-                                    data.append(row)
+                                        for col in change_cols:
+                                            row[col] = int(row[col]) + random.randint(-5, 5)
+                                        # print(row)
+                                        data.append(row)
 
-                            with open(out_file, 'w') as file:
-                                csv_writer = csv.writer(file)
-                                csv_writer.writerows(data)
+                                with open(out_file, 'w') as file:
+                                    csv_writer = csv.writer(file)
+                                    csv_writer.writerows(data)
--- test3.py
+++ test3.py
@@ -26,50 +26,65 @@ saver = tf.train.import_meta_graph(modelpath + 'model.ckpt.meta')
 saver.restore(sess, tf.train.latest_checkpoint(modelpath))
 print(modelpath)
 
-dir_path = 'data3/c/1'
-files = os.listdir(dir_path)
-files.sort()
-for file in files:
-    filerrr = os.path.join(dir_path, file)
-    if os.path.exists(filerrr):  # 归一化
-        df = pd.read_csv(filerrr, header=None)  # 融合用的角度
-        # X_Data=dg.iloc[:,0:6]
-        for iii in range(df.shape[0]):  # 把训练集中的数据按照总的数据归一化
-            for jjj in range(2, 8):
-                tty = df.loc[iii, jjj]
-                df.iloc[iii, jjj] = math.log(tty, 1024)
+dir = 'data3/c/'
 
-        X_Data = df.iloc[:, 2:8]
-        Y_Data = df.iloc[:, 1]
-        # print("XData:", X_Data)
-        # print("Y_Data", Y_Data)
-        X_Data2 = X_Data.as_matrix()
-        X_Data3 = X_Data2.tolist()
+for xx in range(1, 7):
 
-        Y_Data2 = Y_Data.as_matrix()
-        # print(Y_Data2)
-        rrrYTR = Y_Data2.shape[0]
+    dir_path = dir + str(xx)
+    print(dir_path)
 
-        Y_Data3 = Y_Data2.reshape(rrrYTR, 1)
-        Y_Data4 = Y_Data3.tolist()
-        X = None  # input
-        yhat = None  # output
-        X = graph.get_tensor_by_name('input_plllllllllllaceholder_x:0')
-        Y = graph.get_tensor_by_name('ouput_plllllllllllaceholder_y:0')
-        keep_prob = graph.get_tensor_by_name('keep_prob:0')
-        ouputNode = str(outputNodeName) + ':0'
-        yhat = graph.get_tensor_by_name(ouputNode)
-        aac = sess.run(yhat, feed_dict={X: X_Data3, keep_prob: 1.0})
-        df = pd.DataFrame(aac)
+    files = os.listdir(dir_path)
+    files.sort()
+    lines = []
 
-        sum = 0
-        pp = 0
-        for i in range(df.shape[0]):
-            jjji = df.iloc[i, 0]
-            # print("jjji:",jjji)
-            yyyi = Y_Data4[i]
-            # print("yyyi",yyyi)
-            error = abs(yyyi[0] - jjji)
-            sum += error
-        x = sum / df.shape[0]
-        print(str(file) + ' ' + str(x))
+    for file in files:
+        filerrr = os.path.join(dir_path, file)
+        if os.path.exists(filerrr):  # 归一化
+            df = pd.read_csv(filerrr, header=None)  # 融合用的角度
+            # X_Data=dg.iloc[:,0:6]
+            for iii in range(df.shape[0]):  # 把训练集中的数据按照总的数据归一化
+                for jjj in range(2, 8):
+                    tty = df.loc[iii, jjj]
+                    df.iloc[iii, jjj] = math.log(tty, 1024)
+
+            X_Data = df.iloc[:, 2:8]
+            Y_Data = df.iloc[:, 1]
+            # print("XData:", X_Data)
+            # print("Y_Data", Y_Data)
+            X_Data2 = X_Data.as_matrix()
+            X_Data3 = X_Data2.tolist()
+
+            Y_Data2 = Y_Data.as_matrix()
+            # print(Y_Data2)
+            rrrYTR = Y_Data2.shape[0]
+
+            Y_Data3 = Y_Data2.reshape(rrrYTR, 1)
+            Y_Data4 = Y_Data3.tolist()
+            X = None  # input
+            yhat = None  # output
+            X = graph.get_tensor_by_name('input_plllllllllllaceholder_x:0')
+            Y = graph.get_tensor_by_name('ouput_plllllllllllaceholder_y:0')
+            keep_prob = graph.get_tensor_by_name('keep_prob:0')
+            ouputNode = str(outputNodeName) + ':0'
+            yhat = graph.get_tensor_by_name(ouputNode)
+            aac = sess.run(yhat, feed_dict={X: X_Data3, keep_prob: 1.0})
+            df = pd.DataFrame(aac)
+
+            sum = 0
+            pp = 0
+            for i in range(df.shape[0]):
+                jjji = df.iloc[i, 0]
+                # print("jjji:",jjji)
+                yyyi = Y_Data4[i]
+                # print("yyyi",yyyi)
+                error = abs(yyyi[0] - jjji)
+                sum += error
+            x = sum / df.shape[0]
+            line = str(file) + ' ' + str(x) + '\n'
+            print(line)
+            lines.append(line)
+
+    out_file = dir + str(xx) + '.txt'
+    with open(out_file, 'w') as file:
+        file.writelines(lines)
+    print(out_file)
